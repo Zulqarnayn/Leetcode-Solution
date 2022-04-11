f496f1f0149bdadb0c9b4eece585a12432cc55d2
Mujtaba Asif <mujtabaasif04@gmail.com>
2022-04-12 00:34:20 +0600

    Time: 22 ms (26.98%), Space: 7.1 MB (33.92%) - LeetHub

--- 38-count-and-say/38-count-and-say.cpp
+++ 38-count-and-say/38-count-and-say.cpp
@@ -1,5 +1,24 @@
 class Solution {
 public:
+    /*
+    Describe the previous term!
+    1, 11, 21, 1211, 111221, 312211, 12112221, 1112213211, ...
+
+    Just Count and Say 111221 = (111)31(22)21(1)11
+
+    Code Improvements:
+    1. reduce Level Of Indentation Per Method
+    2. Don’t Use The ELSE Keyword
+    
+    you will find more code improvment techniques 
+    here (https://williamdurand.fr/2013/06/03/object-calisthenics/#5-one-dot-per-line)
+    
+    */
+    
+    string concatenate2Number(int a, int b) {
+        return to_string(a) + to_string(b);
+    }
+    
     string sayPrevious(string s) {
         int count = 1, lastNumber = (s[0] - '0'), size = s.size();
         string result = "";
@@ -9,15 +28,18 @@ public:
             bool isLastIndex = idx == size - 1;
             
             if(isLastIndex) {
-                result += (to_string(count) + to_string(lastNumber));
-            } else {
-                if(isConsecutiveEqual) count++;
-                else {
-                    result += (to_string(count) + to_string(lastNumber));
-                    count = 1;
-                    lastNumber = s[idx + 1] - '0';
-                }
-            }
+                result += concatenate2Number(count, lastNumber);
+                break;
+            } 
+            
+            if(isConsecutiveEqual) {
+                count++;
+                continue;
+            } 
+            
+            result += concatenate2Number(count, lastNumber);
+            count = 1;
+            lastNumber = s[idx + 1] - '0';
         }
         
         return result;
