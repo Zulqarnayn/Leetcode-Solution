6c707034be3d3e5a64132fb37f57699a64ee09db
Mujtaba Asif <mujtabaasif04@gmail.com>
2022-04-16 19:57:07 +0600

    Time: 61 ms (34.03%), Space: 33.5 MB (62.62%) - LeetHub

--- 538-convert-bst-to-greater-tree/538-convert-bst-to-greater-tree.cpp
+++ 538-convert-bst-to-greater-tree/538-convert-bst-to-greater-tree.cpp
@@ -10,46 +10,20 @@
  * };
  */
 class Solution {
+    int sum = 0;
 public:
     
-    void inorderTraversal(TreeNode* root, vector<int>& sortedArr) {
+    void inorderTraversal(TreeNode* root) {
         if(root == nullptr) return;
         
-        inorderTraversal(root->left, sortedArr);
-        sortedArr.push_back(root->val);
-        inorderTraversal(root->right, sortedArr);
-    }
-    
-    void updateKeys(TreeNode* root, unordered_map<int, int>& cummSum) {
-        if(root == nullptr) return;
-        
-        root->val = cummSum[root->val];
-        updateKeys(root->left, cummSum);
-        updateKeys(root->right, cummSum);
+        inorderTraversal(root->right);
+        sum += root->val;
+        root->val = sum;
+        inorderTraversal(root->left);
     }
     
     TreeNode* convertBST(TreeNode* root) {
-        /*Idea:
-        1. first converst bst in to sorted array
-        2. map each key value to cummalative sum
-        3. traverse bst and replace mapped value
-        */
-        
-        vector<int> sortedArr;
-        unordered_map<int, int> cummSum;
-        
-        inorderTraversal(root, sortedArr);
-        int size = sortedArr.size();
-        
-        for(int idx = size - 1; idx >= 0; idx--) {
-            if(idx == size - 1) cummSum[sortedArr[idx]] = sortedArr[idx];
-            else {
-               cummSum[sortedArr[idx]] = sortedArr[idx] + cummSum[sortedArr[idx + 1]];
-            }
-        }
-        
-        updateKeys(root, cummSum);
-        
+        inorderTraversal(root);
         return root;
     }
 };
