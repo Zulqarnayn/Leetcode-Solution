263dae12d3278921ae34d629ee5a1363424d1398
Mujtaba Asif <mujtabaasif04@gmail.com>
2022-04-23 13:35:58 +0600

    Time: 733 ms (10.77%), Space: 17.2 MB (24.30%) - LeetHub

--- 45-jump-game-ii/45-jump-game-ii.cpp
+++ 45-jump-game-ii/45-jump-game-ii.cpp
@@ -8,8 +8,12 @@ public:
         
         for(int idx = size - 2; idx >= 0; idx--) {
             for(int i = 1; i <= nums[idx]; i++) {
-                int next_idx = idx + i >= size ? size - 1 : idx + i;
-                dp[idx] = min(dp[idx], 1 + dp[next_idx]);
+                if(idx + i < size) {
+                    dp[idx] = min(dp[idx], 1 + dp[idx + i]);
+                } else {
+                    dp[idx] = min(dp[idx], 1 + dp[size - 1]);
+                    break;
+                }
             }
         }
         
