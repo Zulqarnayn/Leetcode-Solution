9d8d2f8f7b2fa9ab200d27033a93b80264477bbc
Mujtaba Asif <mujtabaasif04@gmail.com>
2022-05-04 06:32:28 +0600

    Time: 211 ms (41.65%), Space: 72.9 MB (16.47%) - LeetHub

--- 1679-max-number-of-k-sum-pairs/1679-max-number-of-k-sum-pairs.cpp
+++ 1679-max-number-of-k-sum-pairs/1679-max-number-of-k-sum-pairs.cpp
@@ -1,6 +1,10 @@
 class Solution {
 public:
     int maxOperations(vector<int>& nums, int k) {
+        /*
+        Approach 1: sort & 2 pointer
+        ----------------------------
+        
         sort(nums.begin(), nums.end());
         
         int size = nums.size(), start = 0, end = size - 1, count = 0;
@@ -16,6 +20,25 @@ public:
                 count++;
                 start++;
                 end--;
+            }
+        }
+        
+        return count;
+        */
+        
+        // Approach 2: frequency count
+        // ----------------------------
+        
+        unordered_map<int, int> freq;
+        int count = 0;
+        
+        for(auto num: nums) {
+            int rem = k - num;
+            if(freq[rem] > 0) {
+                count++;
+                freq[rem]--;
+            } else {
+                freq[num]++;
             }
         }
         
