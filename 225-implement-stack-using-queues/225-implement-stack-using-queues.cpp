31ceda4c7d4b71201951d56846b0d1ced4e99ada
Mujtaba Asif <mujtabaasif04@gmail.com>
2022-05-05 14:42:26 +0600

    Time: 3 ms (35.37%), Space: 6.8 MB (58.19%) - LeetHub

--- 225-implement-stack-using-queues/225-implement-stack-using-queues.cpp
+++ 225-implement-stack-using-queues/225-implement-stack-using-queues.cpp
@@ -1,47 +1,69 @@
 class MyStack {
-    queue<int> qMain, qBackup;
+    // queue<int> qMain, qBackup;
+    //---- second approach ----
+    queue<int> stackQ;
 public:
     MyStack() {
         
     }
     
     void push(int x) {
-        passToBackup(0);
-        qMain.push(x);
+        // passToBackup(0);
+        // qMain.push(x);
+        
+        //---- second approach ---
+        stackQ.push(x);
+        
+        for(int idx = 0; idx < stackQ.size() - 1; idx++) {
+            stackQ.push(stackQ.front());
+            stackQ.pop();
+        }
     }
     
     int pop() {
-        int frontElem = qMain.front();
-        qMain.pop();
-        rearrange();
-        return frontElem;
+        // int frontElem = qMain.front();
+        // qMain.pop();
+        // rearrange();
+        // return frontElem;
+        
+        //---- second approach ---
+        int top = stackQ.front();
+        stackQ.pop();
+        return top;
     }
     
     int top() {
-        int frontElem = qMain.front();
-        return frontElem;
+        // int frontElem = qMain.front();
+        // return frontElem;
+        
+        //---- second approach ---
+        int top = stackQ.front();
+        return top;
     }
     
     bool empty() {
-        return qMain.empty() && qBackup.empty();
+        // return qMain.empty() && qBackup.empty();
+        
+        //---- second approach ---
+        return stackQ.empty();
     }
     
-    void rearrange() {
-        //this method will do 1 thing
-        //if main queue is empty
-        //than swap backup with main and
-        //keep only one number in main
-        swap(qMain, qBackup);
-        passToBackup(1);
-    }
+    // void rearrange() {
+    //     //this method will do 1 thing
+    //     //if main queue is empty
+    //     //than swap backup with main and
+    //     //keep only one number in main
+    //     swap(qMain, qBackup);
+    //     passToBackup(1);
+    // }
     
-    void passToBackup(int limit) {
-        while(qMain.size() > limit) {
-            int frontElem = qMain.front();
-            qBackup.push(frontElem);
-            qMain.pop();
-        }
-    }
+    // void passToBackup(int limit) {
+    //     while(qMain.size() > limit) {
+    //         int frontElem = qMain.front();
+    //         qBackup.push(frontElem);
+    //         qMain.pop();
+    //     }
+    // }
 };
 
 /**
