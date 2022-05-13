5c750c0e365f0b67926e2fffd6cbaf0c378cb7a9
Mujtaba Asif <mujtabaasif04@gmail.com>
2022-05-13 12:04:18 +0600

    Time: 4 ms (99.17%), Space: 10.8 MB (45.74%) - LeetHub

--- 1-two-sum/1-two-sum.cpp
+++ 1-two-sum/1-two-sum.cpp
@@ -2,6 +2,9 @@ class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
         // Q1. is there duplicates
+        
+        // TC: - O(n)
+        // SC: - O(n)
         unordered_map<int, int> uMap;
         
         for(int idx = 0; idx < nums.size(); idx++) {
