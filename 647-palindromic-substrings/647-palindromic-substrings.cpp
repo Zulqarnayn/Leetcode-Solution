2fde0704eaac3e517bd8603adc1d61b06f601555
Mujtaba Asif <mujtabaasif04@gmail.com>
2022-05-22 18:07:06 +0600

    Time: 54 ms (18.82%), Space: 20.1 MB (18.18%) - LeetHub

--- 647-palindromic-substrings/647-palindromic-substrings.cpp
+++ 647-palindromic-substrings/647-palindromic-substrings.cpp
@@ -1,5 +1,8 @@
 class Solution {
 public:
+    /*
+    TC: O(n^3)
+    
     bool isPalindrome(string s, int start, int end) {
         for(int i = start, j = end; i < j; i++, j--) {
             if(s[i] != s[j]) return false;
@@ -18,4 +21,37 @@ public:
         
         return cnt;
     }
+    */
+    
+    vector<vector<int>> dp;
+    
+    int memoization(string &s, int i, int j) {
+        if(i >= j) return 1;
+        if(dp[i][j] != -1) return dp[i][j];
+        
+        int cnt = 0;
+        
+        if(s[i] != s[j]) cnt = 0;
+        else cnt = memoization(s, i + 1, j - 1);
+        
+        return dp[i][j] = cnt;
+    }
+    
+    int countSubstrings(string s) {
+        int sz = s.size();
+        dp.resize(sz, vector<int>(sz, -1));
+        
+        int cnt = 0;
+        for(int i = 0; i < sz; i++) {
+            for(int j = i; j < sz; j++) {
+                cnt += memoization(s, i, j);
+            }
+        }
+        
+        return cnt;
+    }
 };
+
+/*
+"aaabbbcccabc"
+*/
