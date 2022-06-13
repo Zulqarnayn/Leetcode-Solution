10bbf117b49383eccf91fcd66a2a7b505eae3d8d
Mujtaba Asif <mujtabaasif04@gmail.com>
2022-06-13 11:15:37 +0600

    Time: 11 ms (37.61%), Space: 9.1 MB (26.05%) - LeetHub

--- 120-triangle/120-triangle.cpp
+++ 120-triangle/120-triangle.cpp
@@ -1,21 +1,20 @@
 class Solution {
-    int dp[201][201];
+    
 public:
-    int calculateMinPath(int idx, int idy, vector<vector<int>>& triangle) {
-        if(idx <0 || idx >= triangle.size() || idy < 0 || idy >= triangle[idx].size()) return 0;
-        
+    int calculateMinPath(int idx, int idy, vector<vector<int>>& triangle, vector<vector<int>>& dp) {
+        if(idx >= triangle.size() || idy >= triangle[idx].size()) return 0;
         if(dp[idx][idy] != -1) return dp[idx][idy];
         
         int ret1 = 0, ret2 = 0;
         
-        ret1 = triangle[idx][idy] + calculateMinPath(idx+1, idy, triangle);
-        ret2 = triangle[idx][idy] + calculateMinPath(idx+1, idy + 1, triangle);
+        ret1 = triangle[idx][idy] + calculateMinPath(idx+1, idy, triangle, dp);
+        ret2 = triangle[idx][idy] + calculateMinPath(idx+1, idy + 1, triangle, dp);
         
         return dp[idx][idy] = min(ret1, ret2);
     }
     
     int minimumTotal(vector<vector<int>>& triangle) {
-        memset(dp, -1, sizeof(dp));
-        return calculateMinPath(0, 0, triangle);    
+        vector<vector<int>> dp(triangle.size(), vector<int> (triangle.size(), -1));
+        return calculateMinPath(0, 0, triangle, dp);    
     }
 };
