1edb18f1402f0a8f2b75d51ddba8731f7b6a0f51
Mujtaba Asif <mujtabaasif04@gmail.com>
2022-06-15 11:35:20 +0600

    Time: 1712 ms (16.36%), Space: 15 MB (16.36%) - LeetHub

---
+++ 1048-longest-string-chain/1048-longest-string-chain.swift
@@ -0,0 +1,28 @@
+class Solution {
+    private var wordMap: [String: Int] = [:]
+    
+    func longestStrChain(_ words: [String]) -> Int {
+        var sortedWords = words.sorted{ a, b in a.count < b.count }
+        
+        for word in sortedWords {
+            wordMap[word] = 1
+        }
+        
+        var result = 1;
+        
+        for word in sortedWords {
+            for idx in 0..<word.count {
+                let substr = String(word.prefix(idx) + word.suffix(word.count - (idx + 1)))
+                if let val = wordMap[substr] {
+                    wordMap[word] = max(wordMap[word] ?? 1, val + 1)
+                } else {
+                    wordMap[word] = max(wordMap[word] ?? 1, 1);
+                }
+                print(word, substr, wordMap[word], separator: " ")
+            }
+            result = max(result, wordMap[word] ?? 1)
+        }
+        
+        return result;
+    }
+}
